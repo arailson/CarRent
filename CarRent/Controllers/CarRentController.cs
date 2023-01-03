03855f3f49cf6a3511028206ae1ba6ba0994cf3c
Arailson <arailson.vieira@gmail.com>
2023-01-02 21:07:05 -0300

    First commit

---
+++ CarRent/Controllers/CarRentController.cs
@@ -0,0 +1,24 @@
+﻿using CarRent.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRent.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CarRentController
+    {
+        private static List<Store> stores = new List<Store>();
+        
+        [HttpPost]
+        public void AddStore([FromBody] Store store)
+        {
+            stores.Add(store);
+        }
+
+        [HttpGet]
+        public IEnumerable<Store> RecuperaStore()
+        {
+            return stores;
+        }
+    }
+}
---
+++ CarRent/Model/Store.cs
@@ -0,0 +1,10 @@
+﻿namespace CarRent.Model
+{
+    public class Store
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Cnpj { get; set; }
+        public string Cep { get; set; }
+    }
+}
